fbf4780a1b34fad650f8273d042b47f5d9b3f6d1
Dogra-Lalit <124022630+Dogra-Lalit@users.noreply.github.com>
2023-10-04 17:25:35 +0530

    carsouel added

--- index.php
+++ index.php
@@ -20,49 +20,30 @@
      );
      echo generateNavigation($menuItems);
     ?>
-    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
-                aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active" data-bs-interval="10000">
-                <img src="images/chicago.jpg" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
-                </div>
-            </div>
-            <div class="carousel-item" data-bs-interval="2000">
-                <img src="images/la.jpg" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="images/ny.jpg" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
-                </div>
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
+    <?php include 'carousel.php'; 
+    $carouselItems = array(
+        array(
+            'image' =>'images/chicago.jpg',
+            'caption' =>'First slide label',
+            'desc' =>'Some representative placeholder content for the first slide.',
+            'interval'=>'10000',
+        ),
+        array(
+            'image' =>'images/la.jpg',
+            'caption' =>'Second slide label',
+            'desc' =>'Some representative placeholder content for the second slide.',
+            'interval'=>'2000',
+        ),
+        array(
+            'image' =>'images/ny.jpg',
+            'caption' =>'Third slide label',
+            'desc' =>'Some representative placeholder content for the third slide',
+            'interval'=>'',
+        ),
+    );
+    //echo "<pre>";print_r($carouselItems);exit;
+    echo generateCarousel($carouselItems);
+    ?>
     <section class="my-5">
         <div>
             <h3 class="text-center">About Us
